2c0b6b5efd39108eade16aafcd3cac7e415f407d
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-26 12:23:10 +0300

    added list to store data

--- user.py
+++ user.py
@@ -2,7 +2,10 @@ class User:
     """
     class that generates new instance of User
     """
+    user_data = []
 
     def __init__(self,username,password):
         self.username = username
         self.password = password
+
+        
