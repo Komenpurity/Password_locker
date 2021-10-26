16e64c98df50002b53f30e42f7bc6cb229084518
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-26 15:26:24 +0300

    added credentials class

--- credentials.py
+++ credentials.py
@@ -1,5 +1,17 @@
+ class Credentials:
+     """
+     method of generating credentials
+     """
+     user_cred = []
+ 
  def __init__(self,platform,username,password):
      self.platform = platform
      self.username = username
      self.password = password
-     
+    
+def append_cred(self):
+    """
+    method to save credentials
+    """
+    Credentials.user_cred.append(self)
+    
