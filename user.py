08cf67e92071dcf601241dfcc180a3d91cb5a80d
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-26 12:19:12 +0300

    added a function to new generate details

--- user.py
+++ user.py
@@ -2,3 +2,7 @@ class User:
     """
     class that generates new instance of User
     """
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
