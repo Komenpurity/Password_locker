976fdcc7f999d093a3418d1b04ac3a7243c8dd78
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-26 12:28:54 +0300

    added function to store data

--- user.py
+++ user.py
@@ -8,4 +8,13 @@ class User:
         self.username = username
         self.password = password
 
-        
+    def store_data(self):
+        """
+        method to save user in a list
+        """
+        User.user_data.append(self)  
+
+x = User("James","@#$%")
+x.store_data()
+print(x.username)
+print(x.password)
