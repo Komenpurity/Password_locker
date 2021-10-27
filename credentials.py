30df672599a5a65e5844f95d9830afe00b75598b
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-27 07:09:33 +0300

    added generate password method

--- credentials.py
+++ credentials.py
@@ -1,10 +1,13 @@
+ import string
+ import random
+ 
  class Credentials:
      """
      method of generating credentials
      """
      user_cred = []
  
- def __init__(self,platform,username,password):
+def __init__(self,platform,username,password):
      self.platform = platform
      self.username = username
      self.password = password
@@ -14,4 +17,8 @@ def append_cred(self):
     method to save credentials
     """
     Credentials.user_cred.append(self)
-    
+
+@classmethod
+def generate_password(cls,length):
+    characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return "".join(random.choice(characters)for i in range(length))
