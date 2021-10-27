498b162f83a050a2ba08071393a3dbf416af05d5
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-27 07:38:45 +0300

    added login and logout

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from user import User
 
 def main():
     while True:
+        print("sign => to login to app")
+        print("x => to logout")
         user_choice = input().lower()
-        print(user_choice)
+         
         if user_choice == 'sign':
             print("welcome")
         elif user_choice == 'x':
             print("bye")
             break
-            
+
 
 if __name__ == '__main__':
     main()
