1aff47713a21828fb9b5f95a73d8401971a1deb6
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-27 07:51:44 +0300

    added save user fucntion

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from credential import Credentials
 from user import User
 
+def create_user(name, password):
+    """
+    create a new user
+    """
+    new_user = User(username=name, password=password)
+    return new_user
+
+def save_user(user):
+    """
+    save user 
+    """
+    user.append_user()
+
+
 def main():
     while True:
         print("sign => to login to app")
@@ -9,6 +23,9 @@ def main():
          
         if user_choice == 'sign':
             print("welcome")
+            username = input("enter your username: ")
+            password = input("enter your password: ")
+            save_user = (username, password)
         elif user_choice == 'x':
             print("bye")
             break
