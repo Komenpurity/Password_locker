7ee039604b1b8a57875ef89fb6beb13755ceb869
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-27 07:26:35 +0300

    added main fuction

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from credential import Credentials
 from user import User
 
+def main():
+    while True:
+        user_choice = input()
+        print(user_choice)
+if __name__ == '__main__':
+    main()
