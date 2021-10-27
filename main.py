9aa82ca25d6377543a95197f676d3f464b82b2d4
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-27 07:32:21 +0300

    added user choice

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from user import User
 
 def main():
     while True:
-        user_choice = input()
+        user_choice = input().lower()
         print(user_choice)
+        if user_choice == 'sign':
+            print("welcome")
+        elif user_choice == 'x':
+            print("bye")
+            break
+            
+
 if __name__ == '__main__':
     main()
